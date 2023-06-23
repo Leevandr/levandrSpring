ce2d837a9b9908c5bdce79a7d87004434a685ad9
levandr <leva3175@gmail.com>
2023-06-23 18:22:31 +0300

    main

---
+++ src/main/java/ru/levandr/levandrSpring/LevandrSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.levandr.levandrSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevandrSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevandrSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/levandr/levandrSpring/LevandrSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.levandr.levandrSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevandrSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
